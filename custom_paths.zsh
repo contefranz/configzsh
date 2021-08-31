f4e234748523a25229174036ea3a0a248a535ab9
Francesco Grossetti <francesco.grossetti@unibocconi.it>
2021-08-31 16:32:05 +0200

    Added JAVA path

--- custom_paths.zsh
+++ custom_paths.zsh
@@ -4,9 +4,11 @@ export PATH="/usr/local/opt/llvm/bin:$PATH"
 export PATH="/usr/local/opt/gettext/bin:$PATH"
 export PATH="/usr/local/opt/binutils/bin:$PATH"
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+export PATH="/Users/grossetti/miniconda3/bin:$PATH"
+export JAVA_HOME=$(/usr/libexec/java_home)
+#export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-15.0.2.jdk/Contents/Home"
 # this is for miniconda
 # I just don't use this as much...
 # defining JAVA_HOME
-export JAVA_HOME="/usr/libexec/java_home:$JAVA_HOME"
 export CC=/usr/local/bin/gcc-10
 export CPP=/usr/local/bin/g++-10
